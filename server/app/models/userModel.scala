555dbf71a5df8f8952efea46014b53a59c0db167
dreckTrinity <dreck@trinity.edu>
2024-05-04 21:57:48 -0500

    Create userModel.scala

---
+++ server/app/models/userModel.scala
@@ -0,0 +1,19 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'bennettmach',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: 'postgres',
+//   port: 5432,
+// });
+
+class UserModel {
+
+def createUser = ???
+
+def loginUser = ???
+
+def updateScore = ???
+
+}
